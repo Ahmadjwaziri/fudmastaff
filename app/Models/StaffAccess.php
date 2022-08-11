2ed506e5159abc8f66102ba3bf28f9c4951b48be
Ahmad Jafar Waziri <30461886+Ahmadjwaziri@users.noreply.github.com>
2022-08-11 16:08:46 +0100

    staff access model

---
+++ app/Models/StaffAccess.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StaffAccess extends Model
+{
+    use HasFactory;
+}
